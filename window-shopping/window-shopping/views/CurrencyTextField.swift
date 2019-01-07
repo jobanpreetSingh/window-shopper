918c755621f99f6b0411262960f3d65eaf815e4e
Joban Dhot <jobandhot@Jobans-MacBook-Pro.local>
2019-01-06 20:24:49 -0500

    lesson one completed

---
+++ window-shopping/window-shopping/views/CurrencyTextField.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyTextField.swift
+//  window-shopping
+//
+//  Created by Joban Dhot on 2019-01-06.
+//  Copyright © 2019 Joban Dhot. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+    super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2528038727)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        if let p =  placeholder {
+            
+           
+       
+        let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    }
+}
