bf2d18a5e5f9afd01b97112d9ee579e3463e1a2a
Joban Dhot <jobandhot@Jobans-MacBook-Pro.local>
2019-01-06 20:52:33 -0500

    add calculator progmatically

--- window-shopping/window-shopping/controller/MainVC.swift
+++ window-shopping/window-shopping/controller/MainVC.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var wagetxt: CurrencyTextField!
+    
+    @IBOutlet weak var pricetxt: CurrencyTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  ///CREAYTE UIBUTTON PROGMATICALLY///////
+      let calcBtn =  UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60) )
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 0.9449410468)
+        calcBtn.setTitle("Calculate", for: .normal)
+       //  dont use that one because not work so always use .setTitle calcBtn.titleLabel?.textColor = "Calculate"
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate)
+            , for: .touchUpInside)
+        wagetxt.inputAccessoryView = calcBtn
+        pricetxt.inputAccessoryView = calcBtn
     }
-
+    @objc func calculate(){
+        
+        print(" we got here")
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- window-shopping/window-shopping/views/CurrencyTextField.swift
+++ window-shopping/window-shopping/views/CurrencyTextField.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
-
+@IBDesignable
 class CurrencyTextField: UITextField {
-
+    override func prepareForInterfaceBuilder() {
+        custmizeview()
+    }
     override func awakeFromNib() {
     super.awakeFromNib()
+        custmizeview()
+       
+    }
+    func custmizeview(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2528038727)
         layer.cornerRadius = 5.0
         textAlignment = .center
         if let p =  placeholder {
             
-           
-       
-        let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            
+            
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    }
+        }
+        
+        
     }
 }
